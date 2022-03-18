a2bd18f43fa3daa8655ecb5a2a58cad8e1356200
Lucas Coimbra <lucas.a.coimbra@gmail.com>
2022-03-17 23:52:57 -0300

    localhost python changed

--- python-kafka/producer/producer.py
+++ python-kafka/producer/producer.py
@@ -5,7 +5,7 @@ from time import sleep
 from datetime import datetime
 
 # Create an instance of the Kafka producer
-producer = KafkaProducer(bootstrap_servers='localhost:29092',
+producer = KafkaProducer(bootstrap_servers='localhost:9092',
                             value_serializer=lambda v: str(v).encode('utf-8'))
 
 # Call the producer.send method with a producer-record
